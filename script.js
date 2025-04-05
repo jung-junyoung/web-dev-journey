edb4a0e944dfa196cab391b29d1a7e8ce8dc8b2b
Junyoung Jung <junyoungjung@Junyoungs-MacBook-Air.local>
2025-04-05 19:48:12 +0900

    Practice JavaScript variables (let, count)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello from script.js!")
+
+const myName = "Junyoung Jung";
+const currentGoal = "Learning JavaScript variables";
+const currentYear = 2025;
+
+console.log("My Name:", myName);
+console.log("My Goal:", currentGoal);
+console.log("Year:", currentYear);
+
+console.log("Hello, my name is" + myName + " and I am currently" + currentGoal + " in" + currentYear + ".");
